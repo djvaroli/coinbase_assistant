3c92a2d0ce86ed48c936129a42f33f3b4839d006
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-06 18:31:39 -0400

    use a default phone number in sms

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     accounts = fetch_accounts()
     accounts = [str(account) for account in accounts]
     sms_friendly_accounts = "\n".join(accounts)
-    send_sms(to="+18579996528", body=sms_friendly_accounts)
+    send_sms(body=sms_friendly_accounts)
 
 
 
