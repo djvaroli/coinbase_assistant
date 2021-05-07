ca40893d7fad190555e1fe17be3ce7829c4a9d85
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-06 22:53:39 -0400

    Created class to extend Coinbase Account class

--- utils/account_utils.py
+++ utils/account_utils.py
@@ -1,58 +1,80 @@
 from dataclasses import dataclass
 from typing import *
 
+from coinbase.wallet.model import Account
 from utils.coinbase_api_utils import get_coinbase_client
 
 
-@dataclass
-class CoinAccount:
-    """
-    Utility class for Coinbase accounts
-    """
+class ExtendedCoinbaseAccount(object):
     def __init__(
         self,
-        coinbase_account: dict
-    ) -> None:
-        self.name = coinbase_account.name
-        self.balance_amount = float(coinbase_account.balance.amount)
-        self.balance_currency = coinbase_account.balance.currency
-        self.native_balance_amount = float(coinbase_account.native_balance.amount)
-        self.native_balance_currency = coinbase_account.native_balance.currency
+        account: Account
+    ):
+        self.account = account
+
+    def __getattr__(self, attr):
+        print(attr)
+        return getattr(self.account, attr)
+
+    def __repr__(self) -> str:
+        return self.account.__repr__()
+    
+    def __str__(self) -> str:
+        return self.account.__str__()
 
     @property
     def is_active(self):
-        return bool(self.balance_amount)
-
-    @property
-    def account_balance(self):
-        return f"{self.balance_amount} {self.balance_currency}"
-    
-    @property
-    def account_native_balance(self):
-        return f"{self.native_balance_amount} {self.native_balance_currency}"
+        account_balance = self.account.balance.amount
+        return bool(account_balance)
 
     @property
     def summary_info(self) -> str:
-        return f"{self.name} - {self.account_balance} - {self.account_native_balance}"
+        name = self.name
+        balance_amount = self.balance.amount
+        balance_currency = self.balance.currency
+        native_balance_amount = self.native_balance.amount
+        native_balance_currency = self.native_balance.currency
+
+        return f"{name} - {balance_amount} {balance_currency} - {native_balance_amount} {native_balance_currency}"
+
     
-    def __repr__(self) -> str:
-        return self.summary_info
-
-    def __str__(self) -> str:
-        return self.summary_info
-
-
 def fetch_accounts(
     return_active: bool = True
-) -> List[CoinAccount]:
+) -> List[ExtendedCoinbaseAccount]:
 
     client = get_coinbase_client()
-    all_accounts = client.get_accounts().data
+    accounts = client.get_accounts().data
     accounts_to_return = []
-    for account in all_accounts:
-        coin_account = CoinAccount(account)
-        if return_active and coin_account.is_active is False:
+    for account in accounts:
+        account = ExtendedCoinbaseAccount(account)
+        if return_active and account.is_active is False:
             continue
-        accounts_to_return.append(coin_account)
-    
+        accounts_to_return.append(account)
     return accounts_to_return
+
+
+def get_account_net_input_amount(
+    account_id: str = None,
+    account: Optional[Account] = None
+):
+    """
+    returns USD amount equalling cumulative amount of money
+    put into account
+    """
+    assert account_id or account, "Must provide either account_id or instance of Account"
+
+    if account_id and not account:
+        client = get_coinbase_client()
+        account_transactions = client.get_transactions(account_id)
+    else:
+        account_transactions = account.get_transactions()
+    
+    account_net_buy = 0
+    for transaction in account_transactions.data:
+        transaction_type = transaction.type
+        if transaction_type != "buy":
+            continue
+        purchase_amount_usd = float(transaction.native_amount.amount)
+        account_net_buy += purchase_amount_usd
+    
+    return account_net_buy
