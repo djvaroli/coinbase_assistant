9b82f3fc536fb9ca598ab1dd64b874474c6fd57d
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-06 23:30:30 -0400

    testing sending of portfolio info sms

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from utils.messaging_utils import send_sms
 
 if __name__ == "__main__":
     portfolio = Portfolio()
-    portfolio_breakdown = portfolio.breakdown()
-    
-    message = f"Portfolio Net Value: {portfolio_breakdown}"
-    send_sms(body=account_summaries)
+    portfolio_breakdown = portfolio.breakdown(format_for_sms=True)
+
+    send_sms(body=portfolio_breakdown)
 
 
 
