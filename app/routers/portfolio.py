740c916fcb499e360061f21a231b7eec3051aad8
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-08 15:52:01 -0400

    separatae routes into FastAPI routers

---
+++ app/routers/portfolio.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+from utils import portfolio_utils, messaging_utils
+
+router = APIRouter(
+    prefix="/portfolio",
+    tags=["portfolio"],
+    responses={404: {"description": "not found."}}
+)
+
+@router.get("/")
+def get_portfolio():
+    portfolio = portfolio_utils.Portfolio()
+    return portfolio.breakdown()
+
+
+@router.get("/sms_update")
+def send_portfolio_sms_update():
+    portfolio_summary = portfolio_utils.Portfolio().breakdown(format_for_sms=True)
+    message_id = messaging_utils.send_sms(body=portfolio_summary)
+    return message_id
