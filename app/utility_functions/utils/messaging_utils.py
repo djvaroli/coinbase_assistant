6cabae0fbd192ce56cac53efc2ef9b0fa5c218a9
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-08 15:29:02 -0400

    added startup script

---
+++ app/utility_functions/utils/messaging_utils.py
@@ -0,0 +1,31 @@
+import os
+from typing import *
+from utils.twilio_api_utils import get_twilio_client, TWILIO_PHONE_NUMBER
+
+
+DEFAULT_TO_PHONE_NUMBER = os.environ.get("DEFAULT_TO_PHONE_NUMBER")
+
+
+def send_sms(
+    to: str = None,
+    body: str = "",
+    from_: str = TWILIO_PHONE_NUMBER
+):
+    if to is None:
+        to = DEFAULT_TO_PHONE_NUMBER
+    client = get_twilio_client()
+
+    message = client.messages.create(
+        body=body,
+        from_=from_,
+        to=DEFAULT_TO_PHONE_NUMBER
+    )
+    
+    return message.sid
+
+
+def compute_mean(values: List[Union[int, float]]):
+    sum = 0
+    for value in values:
+        sum += value
+    return sum / len(values)
---
+++ scripts/fetch_portfolio_data.sh
@@ -0,0 +1,4 @@
+cd ~/Desktop/coding/coinbase
+python3 -m pip install -r requirements.txt
+python3 -m pip install -e utils/
+python3 scripts/fetch_portfolio_data.py
