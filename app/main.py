376b3c193192fbfdb2cb29f1033cb7679fcc1ee4
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-08 15:50:45 -0400

    project strucutre refactor

--- app/main.py
+++ app/main.py
@@ -3,30 +3,21 @@ from typing import *
 
 from fastapi import FastAPI
 
+from app.routers import portfolio
 from utils.portfolio_utils import Portfolio
 from utils.messaging_utils import send_sms
 
 
 app = FastAPI()
 
+app.include_router(portfolio.router)
 
 @app.get("/")
 def home():
     return "Welcome!"
 
 
-@app.get("/portfolio")
-def get_portfolio():
-    portfolio = Portfolio()
-    
-    return portfolio.breakdown()
 
-
-@app.get("/portfolio/sms_update")
-def send_portfolio_sms_update():
-    portfolio_summary = Portfolio().breakdown(format_for_sms=True)
-    message_id = send_sms(body=portfolio_summary)
-    return message_id
 
     
 
